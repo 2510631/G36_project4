b3c5c0d50827c8fb5afb3169dfeccb6fe614c861
2510631 <145338440+2510631@users.noreply.github.com>
2023-11-16 22:00:02 +0000

    done

--- G36.R
+++ G36.R
@@ -1,19 +1,27 @@
-# Practical 4   Group 36
+# Group 36: Yizhou Huang (s2510631), Houyuan Long (s2485299), Wenhao Yan (s2493934)
+# Github Repo: https://github.com/2510631/G36_project4.git
+
+# Contribution:
+# Yizhou Huang (s2510631): netup + backward functions
+# Houyuan Long (s2485299) : forward function + train 4873
+# Wenhao Yan (s2493934) : train + predict functions
+# We initially analysed and discussed the problem and made comments together.
 
 # The aim of this project is to write functions to set up a neural network for 
 # classification, and to train it using stochastic gradient descent. More specifically,
 # the project implement a neural network for the classification of iris species 
 # based on their characteristics.
 
-# Five functions can be seen in this project, including 'netup' function for setting
-# up the network architecture, 'forward' and 'backward' functions for performing
-# forward and backward propagation, 'train' function for training the network and 
-# 'predict' function for making predictions on new data.
+# Five functions can be seen in this project, including 'netup' function for 
+# setting up the network architecture, 'forward' and 'backward' functions for 
+# performing forward and backward propagation, 'train' function for training the
+# network and 'predict' function for making predictions on new data.
 
 
 netup = function (d){
   # The purpose of this function is to initialize a neural network with the 
-  # specified layer sizes, providing initial values for nodes, weight matrices, and offset vectors.
+  # specified layer sizes, providing initial values for nodes, weight matrices, 
+  # and offset vectors.
   
   # Input:
   # d: A vector specifying the number of nodes in each layer of the neural network.
@@ -24,7 +32,6 @@ netup = function (d){
   # 'W': A list of matrices representing the weight matrices linking consecutive layers.
   # 'b': A list of vectors representing the offset vectors linking consecutive layers.
   # 'd': The input vector specifying the layer sizes.
-  
   
   
   # Initialize lists for nodes 'h', weight matrices 'W', and offset vectors 'b'
@@ -45,6 +52,8 @@ netup = function (d){
   # Return a list containing nodes 'h', weight matrices 'W', offset vectors 'b', and the layer sizes 'd'
   return(list(h = h, W = W, b = b, d = d))
 }
+
+
 
 
 forward <- function(nn,inp){
@@ -70,10 +79,11 @@ forward <- function(nn,inp){
   
   # Set the input values for the first layer
   h[1] <- list(inp)
+  
   for (i in 1:length(b)){
     
     # Compute the linear transformation
-    z=matrix(unlist(W[i]),d[i+1]) %*% unlist(h[i]) + unlist(b[i])
+    z <- matrix(unlist(W[i]),d[i+1]) %*% unlist(h[i]) + unlist(b[i])
     
     # Apply the ReLU activation function
     z[z < 0] <- 0
@@ -85,6 +95,8 @@ forward <- function(nn,inp){
   # Return the updated network list
   return(nn = list(h=h,W=W,b=b,d=d)) 
 }
+
+
 
 backward=function(nn,k){ 
   # The purpose of the backward function is to compute derivatives of the loss 
@@ -106,10 +118,10 @@ backward=function(nn,k){
   d <- nn$d
   
   # Get the output values for the last layer
-  output=unlist(h[length(d)])
+  output <- unlist(h[length(d)])
   
   # Initialize lists for derivatives dh, dW, and db
-  dW=dh=db=list()
+  dW <- dh <- db <- list()
   
   # Iterate through layers in reverse order to compute derivatives using 
   # back-propagation
@@ -126,19 +138,22 @@ backward=function(nn,k){
       
       # Compute derivatives w.r.t. nodes, weights, and offsets for each layer 
       # using back-propagation
-      d_l1=unlist(dh[i+1])*(unlist(h[i+1])>0)
+      d_l1 <- unlist(dh[i+1])*(unlist(h[i+1])>0)
       
-      dh[i]=list(t(matrix(unlist(W[i]),d[i+1]))%*%d_l1)
+      dh[i] <- list(t(matrix(unlist(W[i]),d[i+1]))%*%d_l1)
       
-      dW[i]=list(matrix(d_l1,d[i+1]) %*% matrix((unlist(h[i])),ncol=d[i]))
+      dW[i] <- list(matrix(d_l1,d[i+1]) %*% matrix((unlist(h[i])),ncol=d[i]))
       
-      db[i]=list(d_l1)
+      db[i] <- list(d_l1)
     }
   }
   
   # Return the updated network list with derivatives
   return(list(h=h,W=W,b=b,d=d,dh=dh,dW=dW,db=db))
 }
+
+
+
 
 train=function(nn,inp,k,eta=.01,mb=10,nstep=10000){ 
   # The aim of the train function is to train a neural network using stochastic gradient descent.
@@ -161,41 +176,41 @@ train=function(nn,inp,k,eta=.01,mb=10,nstep=10000){
   for (i in 1:nstep){
     
     # Randomly sample a minibatch of data
-    index=sample(dim(inp)[1],mb)
-    data_train=inp[index,]
-    k_train=k[index]
+    index <- sample(dim(inp)[1],mb)
+    data_train <- inp[index,]
+    k_train <- k[index]
     
     # Extract elements from the network list
-    d=nn$d ;b=nn$b ;W=nn$W 
+    d <- nn$d ;b <- nn$b ;W <- nn$W 
     
     # Initialize lists for averaging gradients
-    aver_dW=list()
-    aver_db=list()
+    aver_dW <- list()
+    aver_db <- list()
     
     
     for (j in 1:length(b)){
       
       # Initialize averaged gradients
-      aver_dW[j]=list(matrix(0,d[j],d[j+1]))
-      aver_db[j]=list(rep(0,d[j+1]))
+      aver_dW[j] <- list(matrix(0,d[j],d[j+1]))
+      aver_db[j] <- list(rep(0,d[j+1]))
     }
     
     # Compute gradients and accumulate for each sample in the minibatch
     for (s in 1:mb){
       
       # Do the forward and backward propagation for each sample in the minibatch
-      nn_1=forward(nn,data_train[s,])
-      nn_1=backward(nn_1,k_train[s])
+      nn_1 <- forward(nn,data_train[s,])
+      nn_1 <- backward(nn_1,k_train[s])
       
       # Update dW and db
-      dW=nn_1$dW ;db=nn_1$db
+      dW <- nn_1$dW ;db <- nn_1$db
       
       # Accumulate gradients
       for (j in 1:length(b)){ 
         
         # Compute the average gradients for each layer
-        aver_dW[j]=list(matrix(unlist(dW[j]),d[j])/mb+matrix(unlist(aver_dW[j]),d[j]))
-        aver_db[j]=list(unlist(db[j])/mb+unlist(aver_db[j]))
+        aver_dW[j] <- list(matrix(unlist(dW[j]),d[j])/mb+matrix(unlist(aver_dW[j]),d[j]))
+        aver_db[j] <- list(unlist(db[j])/mb+unlist(aver_db[j]))
       }
     }
     
@@ -203,19 +218,22 @@ train=function(nn,inp,k,eta=.01,mb=10,nstep=10000){
     for (j in 1:length(b)){
       
       # Update weights
-      W[j]=list(matrix(unlist(W[j]),d[j])-eta*matrix(unlist(aver_dW[j]),d[j]))
+      W[j] <- list(matrix(unlist(W[j]),d[j]) - eta * matrix(unlist(aver_dW[j]),d[j]))
       
       # Update offsets
-      b[j]= list(unlist(b[j])-eta*unlist(aver_db[j]))
+      b[j] <- list(unlist(b[j]) - eta * unlist(aver_db[j]))
     }
     
     # Update the network list with the newest parameters
-    nn$W=W ;nn$b=b
+    nn$W <- W ; nn$b <- b
   }  
   
   # Return the updated network list after training
   return(nn)
 }
+
+
+
 
 
 predict=function(nn,inp){
@@ -235,22 +253,22 @@ predict=function(nn,inp){
   
   
   # Set up an empty vector to store predictions later
-  prediction=c()
+  prediction <- c()
   
   
   for (i in 1:dim(inp)[1]){
     
     # Do the forward propagation to get the node values of the output layer
-    h=forward(nn,inp[i,])$h
+    h <- forward(nn,inp[i,])$h
     
     # Extract the output layer node values and convert to a vector
-    h=unlist(h[length(h)])
+    h <- unlist(h[length(h)])
     
     # Obtain the probabilities
-    p=exp(h)/sum(exp(h))
+    p <- exp(h)/sum(exp(h))
     
-    # Set the predicted class as the most probable one.
-    prediction[i]= which.max(p)
+    # Set the predicted class as the most probable one
+    prediction[i] <- which.max(p)
   }
   
   # Return the vector of predictions
@@ -259,31 +277,32 @@ predict=function(nn,inp){
 
 
 # Set the network to be 4,8,7,3
-d=c(4,8,7,3)
+d <- c(4,8,7,3)
 
 # Assign numeric labels to iris species
 levels(iris[,5])=1:3
-label=as.numeric(iris[,5])
+label <- as.numeric(iris[,5])
 
 # Obtain training data by excluding every 5th row
-train_data=iris[-seq(5,150,5),1:4]
-train_k=label[-seq(5,150,5)]
+train_data <- iris[-seq(5,150,5),1:4]
+train_k <- label[-seq(5,150,5)]
 
 # Obtain test data by using every 5th row
-test_data=iris[seq(5,150,5),1:4]
-test_k=label[seq(5,150,5)]
+test_data <- iris[seq(5,150,5),1:4]
+test_k <- label[seq(5,150,5)]
 
-# Set seed for reporducibility during training
+# Set seed for reproducibility during training
 set.seed(6666)
 
 # Train the neural network using stochastic gradient descent
-nn=train(netup(d),train_data,train_k,nstep=10000)
+nn <- train(netup(d), train_data, train_k, nstep=10000)
 print(nn)
 
 # Predict iris species on the test data by using the trained network
-prediction=predict(nn,test_data)
+prediction <- predict(nn, test_data)
 print(prediction)
 
 # Calculate the accuracy
-accuracy=mean(prediction==test_k)
+accuracy <- mean(prediction == test_k)
 print(accuracy)
+
