5dde278f63bbce8c8addab6f56a9b05e088152c4
2510631 <1034550649@qq.com>
2023-11-16 11:40:43 +0000

    added netup

--- G36.R
+++ G36.R
@@ -1,1 +1,15 @@
 #G36
+
+netup = function (d){
+  h <- W <- b <- list()
+  
+  for( i in 1:(length(d) - 1)){
+    
+    h[i]=list(rep(0, d[i]))
+    
+    W[i]=list(matrix(runif(d[i] * d[i+1], 0, 0.2),d[i+1]))
+    
+    b[i]=list(runif(d[i+1], 0, 0.2))    
+  }
+  return(list(h = h, W = W, b = b, d = d))
+}
